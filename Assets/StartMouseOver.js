b5f0de906f103594cf1791e1b71087793c0a85a7
dhanwright <dylanhanwright@gmail.com>
2015-01-24 13:52:15 -0800

    assets

---
+++ Assets/StartMouseOver.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var newSprite : GameObject;
+var currentSpritePosition : Vector3;
+
+function OnMouseEnter(){
+
+  //getting the current position of the current sprite if ever it can move;
+  var currentSpritePosition = transform.position;
+
+  //then make it invisible
+  renderer.enabled = false;
+
+  //give the new sprite the position of the latter
+  newSprite.transform.position = currentSpritePosition;
+
+  //then make it visible
+  newSprite.renderer.enabled = true;
+}
+
+function OnMouseExit(){
+  //just the reverse process
+  renderer.enabled = true;
+  newSprite.renderer.enabled = false;
+}
---
+++ Assets/scroll.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+
+function Update () {
+	
+	var mr : MeshRenderer = GetComponent(MeshRenderer);
+	
+	var mat : Material = mr.sharedMaterial;
+	
+	var offset : Vector2 = mat.mainTextureOffset;
+	
+	offset.x += Time.deltaTime / 15;
+	
+	mat.mainTextureOffset = offset;
+}
