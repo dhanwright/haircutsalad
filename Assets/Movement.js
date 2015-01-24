2a295f5f5c75db5dd3655edd976a57ad4934f13e
dhanwright <dylanhanwright@gmail.com>
2015-01-23 23:29:26 -0800

    dope

---
+++ Assets/Movement.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	var horizontal = Input.GetAxis("Horizontal");
+	if(Mathf.Abs(horizontal) > 0.9){
+		rigidbody.velocity.x = horizontal * 15;
+	}
+	var vertical = Input.GetAxis("Vertical");
+	if(Mathf.Abs(vertical) > 0.9){
+		rigidbody.velocity.y = vertical * 15;
+	}
+}
