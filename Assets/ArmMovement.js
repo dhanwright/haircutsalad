f4db3847a61ecf29c4acd91de7bb57f85c7aa139
tfendall <troy.fendall@gmail.com>
2015-01-24 16:29:05 -0800

    Arms moving in and out

---
+++ Assets/ArmMovement.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+var speed            = 50;
+var horizontal_input = "RHorizontal";
+var vertical_input   = "RVertical";
+var depth_in_input   = "Depth inR";
+var depth_out_input  = "Depth outR";
+var y_clamp          = new Vector2(-6, 8);
+var x_clamp          = new Vector2(9, 21);
+var z_clamp          = new Vector2(-4, 1.2);
+
+function Update () {
+    // Move left and right
+    var horizontal = Input.GetAxis(horizontal_input);
+    if(Mathf.Abs(horizontal) > 0.9){
+        rigidbody.velocity.x = horizontal * speed;
+    }
+
+    // Move up and down
+    var vertical = Input.GetAxis(vertical_input);
+    if(Mathf.Abs(vertical) > 0.9){
+        rigidbody.velocity.y = vertical * speed;
+    }
+
+    // Move in and out
+    var depth_out = Input.GetAxis(depth_out_input);
+    if (depth_out > 0) {
+        rigidbody.AddRelativeForce(Vector3.forward * speed * 1000);
+    }
+    var depth_in = Input.GetButton(depth_in_input);
+    if (depth_in) {
+        rigidbody.AddRelativeForce(Vector3.back * speed * 1000);
+    }
+
+    transform.position.y = Mathf.Clamp(transform.position.y, y_clamp.x, y_clamp.y);
+    transform.position.x = Mathf.Clamp(transform.position.x, x_clamp.x, x_clamp.y);
+    transform.position.z = Mathf.Clamp(transform.position.z, z_clamp.x, z_clamp.y);
+}
