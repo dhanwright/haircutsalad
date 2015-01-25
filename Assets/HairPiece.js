5e0890eaf68fe1809d76bdb4e8eaa513a4844cd3
tfendall <troy.fendall@gmail.com>
2015-01-24 20:05:07 -0800

    Adding hair blocks

---
+++ Assets/HairPiece.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+function OnTriggerEnter(other : Collider) {
+    Debug.Log("HAIR! " + other.name);
+    if (other.name == "Cylinder R" || other.name == "Cylinder L") {
+        Debug.Log("FLY!");
+        rigidbody.useGravity = true;
+        rigidbody.AddRelativeForce(new Vector3(Random.Range(-300,300), Random.Range(100, 300), 0) * 2);
+    }
+}
