91ca875329dc3c69c64586398df2d158f79b6879
tfendall <troy.fendall@gmail.com>
2015-01-25 01:02:26 -0800

    some shit

---
+++ Assets/GameTime.js
@@ -0,0 +1,59 @@
+﻿#pragma strict
+
+static var cuttingHair = true;
+
+private var _isCustomerEntering = false;
+private var _customerStartTime:float;
+private var _customerStartPos:Transform;
+private var _customer:GameObject;
+
+var customerSeatedPos:Transform;
+var distanceToSeat:float = 18;
+
+var customerEntranceSpeed = 5.0;
+
+
+function Start () {
+  //newCustomer();
+
+}
+
+function Update () {
+  if(_isCustomerEntering)
+  {
+    //customer animates in
+    var distanceTraveled = (Time.time - _customerStartTime) * customerEntranceSpeed;
+    var percentThere = distanceTraveled/distanceToSeat;
+    _customer.transform.position = Vector3.Lerp(_customerStartPos.position, customerSeatedPos.position, percentThere);
+
+    //slide in interface with goal objective for this sick cut as customer enters
+
+    //reds to cut!
+    if(_customer.transform.position == customerSeatedPos.position)
+    {
+      _isCustomerEntering = false;
+      startCutting();
+    }
+  }
+
+}
+
+function newCustomer() {
+  _customer = GameObject("Head");
+  _isCustomerEntering = true;
+  _customerStartTime = Time.time;
+  _customerStartPos = customerSeatedPos;
+  _customerStartPos.position.y -= distanceToSeat;
+
+  //customer enters screen
+
+  //set haircut request (20-100%)
+
+
+}
+
+function startCutting() {
+  cuttingHair = true;
+
+  //display tooltip to press start to end cut
+}
--- Assets/Scripts/ArmMovement.js
+++ Assets/Scripts/ArmMovement.js
@@ -10,30 +10,33 @@ var x_clamp          = new Vector2(9, 21);
 var z_clamp          = new Vector2(-4, 1.2);
 
 function Update () {
-    // Move left and right
-    var horizontal = Controller.getInput(horizontal_input);
-    if(Mathf.Abs(horizontal) > 0.9){
-        rigidbody.velocity.x = horizontal * speed;
-    }
+    if(GameTime.cuttingHair)
+    {
+        // Move left and right
+        var horizontal = Controller.getInput(horizontal_input);
+        if(Mathf.Abs(horizontal) > 0.9){
+            rigidbody.velocity.x = horizontal * speed;
+        }
 
-    // Move up and down
-    var vertical = Controller.getInput(vertical_input);
-    if(Mathf.Abs(vertical) > 0.9){
-        rigidbody.velocity.y = -vertical * speed;
-    }
+        // Move up and down
+        var vertical = Controller.getInput(vertical_input);
+        if(Mathf.Abs(vertical) > 0.9){
+            rigidbody.velocity.y = -vertical * speed;
+        }
 
-    // Move in and out
-    var depth_out = Controller.getInput(depth_out_input);
-    if (depth_out > 0) {
-        rigidbody.AddRelativeForce(Vector3.forward * speed * 1000);
-    }
-    var depth_in = Controller.getInput(depth_in_input);
-    if (depth_in) {
-        rigidbody.AddRelativeForce(Vector3.back * speed * 1000);
-    }
+        // Move in and out
+        var depth_out = Controller.getInput(depth_out_input);
+        if (depth_out > 0) {
+            rigidbody.AddRelativeForce(Vector3.forward * speed * 1000);
+        }
+        var depth_in = Controller.getInput(depth_in_input);
+        if (depth_in) {
+            rigidbody.AddRelativeForce(Vector3.back * speed * 1000);
+        }
 
-    transform.position.y = Mathf.Clamp(transform.position.y, y_clamp.x, y_clamp.y);
-    transform.position.x = Mathf.Clamp(transform.position.x, x_clamp.x, x_clamp.y);
-    transform.position.z = Mathf.Clamp(transform.position.z, z_clamp.x, z_clamp.y);
+        transform.position.y = Mathf.Clamp(transform.position.y, y_clamp.x, y_clamp.y);
+        transform.position.x = Mathf.Clamp(transform.position.x, x_clamp.x, x_clamp.y);
+        transform.position.z = Mathf.Clamp(transform.position.z, z_clamp.x, z_clamp.y);
+    }
    
 }
