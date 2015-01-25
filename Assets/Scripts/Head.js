846c2a97883a833811bfe3c639d60d7d65d55d08
tfendall <troy.fendall@gmail.com>
2015-01-25 13:12:52 -0800

    Adding flat top

--- Assets/Scripts/Head.js
+++ Assets/Scripts/Head.js
@@ -1,7 +1,13 @@
 ﻿#pragma strict
 
 function Start () {
-    generateFlatTop();
+    var rand = Random.Range(0,100);
+
+    if (rand < 50) {
+        generateMohawk();
+    } else {
+        generateFlatTop();
+    }
 }
 
 function Update () {
@@ -11,6 +17,30 @@ function Update () {
 var hairPiece : HairPiece;
 
 function generateFlatTop() {
+    var numberOfObjects = 22;
+    var radius_start = 1.7;
+    var radius_increment = hairPiece.transform.lossyScale.y;
+    var y_start = 3.4;
+    var y_end = 6;
+    var y_increment = (hairPiece.transform.lossyScale.y*0.8);
+
+    for (var y = y_start; y < y_end; y += y_increment) {
+        for (var radius = radius_start; radius > 0; radius -= radius_increment) {
+            var no = numberOfObjects*radius;
+            for (var i = 0; i < no; i++) {
+                var angle = i * Mathf.PI * 2 / no;
+                var pos = Vector3 (Mathf.Cos(angle), 0, Mathf.Sin(angle)) * radius;
+                var hp = Instantiate(hairPiece);
+
+                hp.transform.parent = transform;
+                hp.transform.position = pos;
+                hp.transform.position.y = y;
+            }
+        }
+    }
+}
+
+function generateMohawk() {
     var numberOfLayers = 4;
     var radius_start = 4;
     var radius_increment = hairPiece.transform.lossyScale.x;
